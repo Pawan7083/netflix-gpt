8e9dce16c9338940306b328674514d651a10b4f4
Pawan Kumar <pawankr7083@gmail.com>
2025-07-23 23:40:18 +0530

    Login page updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import { LANDING_IMG, LOGO_IMG } from "../utils/constraint";
+
 const Header = ()=>{
 
     return (
-        <div className="font-bold text-xl text-center">this is eader component</div>
+        <div className="absolute z-50">
+            <img src={LOGO_IMG} alt="logo image not found" className="w-40 -mt-8 "></img>
+        </div>
     )
 }
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import { LANDING_IMG } from "../utils/constraint";
 import { useState ,useRef} from "react";
+import Header from "./Header";
 
 const Login = ()=>{
 
@@ -10,29 +11,39 @@ const Login = ()=>{
     }
 
     const name=useRef();
-    const email=useRef();
-    const password=useRef();
+    const email=useRef(null);
+    const password=useRef(null);
     console.log(email );
+    console.log(password );
+
+    const handlerSubmitButton =()=>{
+        console.log(email );
+    console.log(password );
+    }
 
     return(
-        <div className="relative">
+        <div className="">
+            <Header/>
             <div className="absolute">
                 <img src={LANDING_IMG} alt="Image not found" className="w-screen h-screen object-cover"></img>
             </div>
-            
-                <div className="absolute w-4/12 bg-gray-950 my-24 mx-120 text-white p-4 rounded-2xl opacity-90">
-                    <h1 className="font-bold text-3xl m-6 text-white">{signStatus?"Sign In":"Sign Up"}</h1>
-                    <form >
-                        {!signStatus && <input ref={name} type="text" placeholder="Enter your name" className="mx-6 my-2 p-4 w-10/12 border-[0.5px] rounded-l"/>}
-                        <input ref={email} type="email" placeholder="Enter your email" className="mx-6 my-2 p-4 w-10/12 border-[0.5px] rounded-l"/>
-                        <input ref={password} type="password" placeholder="Enter your password" className="mx-6 my-2 p-4 w-10/12 border-[0.5px] rounded-l"/>
-                        <button className=" bg-red-700 mx-6 my-2 p-2 cursor-pointer w-10/12 border-[0.5px] rounded-l">Submit</button>
-                        <button className="  mx-6 my-2 p-2 cursor-pointer w-10/12 ">forgot password?</button>
-                        <div onClick={handleSignStatus} className=" mx-6 my-2 p-2 cursor-pointer w-10/12 underline text-xl">{signStatus? "new to netflix? Sign Up":"already account? Sign In"}</div>
-                        
-                        
-                    </form>
-                </div>
+            <form onSubmit={(e)=>e.preventDefault()} className="absolute w-4/12 bg-gray-950 my-30 mx-120 text-white p-4 rounded-2xl opacity-90">
+                
+                <h1 className="font-bold text-3xl m-6 text-white">{signStatus?"Sign In":"Sign Up"}</h1>
+                {!signStatus && <input ref={name} type="text" placeholder="Enter your name" className="mx-6 my-2 p-4 w-10/12 border-[0.5px] rounded-l"/>}
+                
+                <input ref={email} type="email" placeholder="Enter your email" className="mx-6 my-2 p-4 w-10/12 border-[0.5px] rounded-l"/>
+                
+                <input ref={password} type="password" placeholder="Enter your password" className="mx-6 my-2 p-4 w-10/12 border-[0.5px] rounded-l"/>
+                
+                <button onClick={handlerSubmitButton} className=" bg-red-700 mx-6 my-2 p-2 cursor-pointer w-10/12 border-[0.5px] rounded-l">Submit</button>
+                
+                <button className="  mx-6 my-2 p-2 cursor-pointer w-10/12 ">forgot password?</button>
+                <div onClick={handleSignStatus} className=" mx-6 my-2 p-2 cursor-pointer w-10/12 underline text-xl">{signStatus? "new to netflix? Sign Up":"already account? Sign In"}</div>
+                
+                
+            </form>
+               
             
         </div>
     )
