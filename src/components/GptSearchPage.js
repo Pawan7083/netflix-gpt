9f78d51b2b0afd6cf21ff508ab62c2c2489a1841
Pawan Kumar <pawankr7083@gmail.com>
2025-08-02 23:54:56 +0530

    Changed in GPT search bar

--- src/components/GptSearchPage.js
+++ src/components/GptSearchPage.js
@@ -1,7 +1,10 @@
+import { LANDING_IMG } from "../utils/constraint";
+
 const GptSearchPage = ()=>{
 
     return (
         <div>
+            <img src={LANDING_IMG} className="w-screen h-screen absolute"></img>
             <input type="text" className="w-1/2 p-2 border border-gray-500"/>
             <button className="px-4 py-2 bg-gray-400">Search</button>
         </div>
